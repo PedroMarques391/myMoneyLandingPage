2b81a4e86e90a10acd42e8adcc43fd14024cd22b
Pedro <pedromarques391@gmail.com>
2024-12-04 09:53:40 -0300

    feat: Implementando responsividade no Hero da aplicação.

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -6,13 +6,15 @@
     class="grid grid-cols-1 md:grid-cols-2 bg-white/50 w-full rounded-2xl overflow-hidden"
 >
     <section
-        class="flex flex-col justify-center items-center px-5 text-black/80"
+        class="flex flex-col justify-center items-center px-5 text-black/80 order-2 md:order-1"
     >
-        <div class="px-5 w-4/5">
-            <h1 class="text-3xl font-semibold tracking-wider">
+        <div class="px-5 w-full md:w-4/5 my-10 md:my-0">
+            <h1
+                class="text-2xl md:text-3xl font-semibold tracking-wider text-[#189ad3] leading-6 md:leading-none"
+            >
                 Controle suas finanças como nunca antes
             </h1>
-            <h3 class="text-sm font-sans font-medium">
+            <h3 class="text-sm font-sans font-medium mt-2">
                 O <b>MyMoney</b> é seu assistente fincanceiro, projetado para ajudar
                 você a registrar, monitorar e organizar suas despesas e receitas
                 de maneira eficaz. Transforme sua gestão financeira em algo simples
@@ -20,14 +22,14 @@
             </h3>
             <div class="my-4 flex flex-row gap-3">
                 <button
-                    class="bg-[#189ad3] hover:bg-[#71c7ec] transition-all duration-300 text-white font-semibold p-3 w-1/2 rounded-2xl flex items-center justify-center space-x-2"
+                    class="bg-[#189ad3] hover:bg-[#71c7ec] transition-all duration-300 text-white font-semibold p-2 md:p-3 flex-1 rounded-2xl flex items-center justify-center space-x-2 text-sm md:text-base"
                     aria-label="Baixar APP"
                 >
                     <p>Baixar APP</p>
                     <BrandGooglePlay class="w-5 h-5" />
                 </button>
                 <button
-                    class="border border-[#189ad3] p-3 w-1/2 rounded-2xl text-[#189ad3] font-semibold hover:bg-[#71c7ec] hover:text-white transition-all duration-300"
+                    class="border border-[#189ad3] p-2 md:p-3 flex-1 rounded-2xl text-[#189ad3] font-semibold hover:bg-[#71c7ec] hover:text-white transition-all duration-300 text-sm md:text-base"
                     >Saiba mais</button
                 >
             </div>
@@ -48,11 +50,7 @@
             </ul>
         </div>
     </section>
-    <section class="flex flex-col justify-center items-end mt-10">
-        <img
-            src="/mmHero.png"
-            alt="Foto da tela inicial do aplicativo"
-            class="w-4/5 object-cover"
-        />
+    <section class="flex flex-col items-end mt-10 order-1 md:order-2">
+        <img src="/mmHero2.png" alt="Foto da tela inicial do aplicativo" />
     </section>
 </main>
